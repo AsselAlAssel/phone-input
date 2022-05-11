4f9d1709a2a0c73fa2966872e679e7ea224f01f1
Assel Al Assel <aseel1234500000@gmail.com>
2022-05-11 23:01:27 +0300

    cretae input and style it

---
+++ src/components/DropListInput.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import  "./DropListInput.css"
+
+type Props={
+  code:string
+}
+const DropListInput:React.FC<Props> = ({code}) => {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const changePhoneInputHandler=(event:any)=>{
+    const enteredValue:string=event.target.value;
+    const deleteFormInputAndTheLastCharIsHyphen=enteredValue.length>=3&&enteredValue.at(-1)==="-";
+    if(deleteFormInputAndTheLastCharIsHyphen){
+      setInputValue(enteredValue.slice(0,-2));
+    }
+  const pressNumberNotCharacter=!isNaN(Number(enteredValue.at(-1)))|| enteredValue.length===0;
+    if(pressNumberNotCharacter){
+           setInputValue(enteredValue);
+    }
+  }
+
+  useEffect(() => {
+    const inputValueLengthDivedBy3withoutHyphen= inputValue.length < 11 && inputValue.replaceAll("-", "").length % 3 === 0 && inputValue.at(-1) !== "-";;
+    if(inputValueLengthDivedBy3withoutHyphen && inputValue){
+      setInputValue(preInputValue=>preInputValue+"-");
+    }
+  }, [inputValue])
+  
+  return (
+    <div className="dropListInput">
+            <span className='dropListInput__code'>({code})</span>
+            <input
+                type="text"
+                value={inputValue}
+                placeholder="xxx-xxx-xxx"
+                maxLength={11}
+                className="dropListInput__inputNumber"               
+                onChange={changePhoneInputHandler}
+            />
+
+        </div>
+  )
+}
+
+export default DropListInput;

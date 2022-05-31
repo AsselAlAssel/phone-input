b4c50a9b92db5e112ed17319c5e4ee6507557ce5
Assel Al-Assel <aseel1234500000@gmail.com>
2022-06-01 00:04:54 +0300

    add countries to json file and read it then use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React, { useState } from "react";
 import { dropListValueType } from "./components/types/types";
-import useFetchData from "./components/CustomHook/usefetch";
-import { URL } from "./components/constant/Constant";
+import countriesData from './components/constant/countries.json'
 import DropListContainer from "./components/DropListContainer/DropListContainer";
 
 const App: React.VFC = () => {
-
-  const { data: countriesCode, loading, hasError } = useFetchData<dropListValueType>(URL);
-  console.log(countriesCode, loading,hasError);
   const [selectedCountry, setSelectedCountry] = useState<dropListValueType>();
 
   const handelSelectedValueChange = (newCountrySelected: dropListValueType) => {
     setSelectedCountry(newCountrySelected);
   }
   return <DropListContainer
-    countriesCode={countriesCode}
+    countriesCode={countriesData}
     selectedCountry={selectedCountry}
     onChangeSelectedValue={handelSelectedValueChange}
-    loading={loading}
-    hasError={hasError}
+    loading={false}
+    hasError={true}
   />;
 };
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import ReactDom from "react-dom";
 import App from "./App";
-import React from "react";
+
 ReactDom.render(<App />, document.getElementById("root"));
